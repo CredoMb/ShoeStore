bce808cc0b59b20afe44d1d44575a47735db8bd5
Credo-Mbikayi <credo.mbikayi@umontreal.ca>
2021-05-21 13:20:57 -0400

    Complete the code of the save button of the details fragment

--- app/src/main/java/com/udacity/shoestore/screens/details/DetailsFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/details/DetailsFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.DetailsFragmentBinding
@@ -47,8 +48,6 @@ class DetailsFragment : Fragment() {
         viewModel = ViewModelProvider(this, viewModelFactory)
                 .get(DetailsViewModel::class.java)
 
-        //
-        //
         //Log.i("DetailActivity","é pozition yango "+detailsFragmentArgs.position.toString())
 
         // If the shoe is empty name, cie, size and description
@@ -64,13 +63,34 @@ class DetailsFragment : Fragment() {
                     !(binding.editShoeSize.text!!.isEmpty())
                ){
 
+                   var cieName = ""
+                   var descript = ""
+
+                if(!binding.editCompanyName.text.toString().isEmpty()){
+                    cieName = binding.editCompanyName.text.toString()
+                }
+
+                if(!binding.editShoeDescription.text.toString().isEmpty()){
+                    descript = binding.editShoeDescription.text.toString()
+                }
+
                 // Get all the fields
                 // Create a new shoes and add it to the
                 // list
-
-
+                Shoe(binding.editShoeName.text!!.toString(),
+                    binding.editShoeSize.text!!.toString().toDouble(),
+                    cieName,descript)
 
                 // 1) Create the layout
+
+                // send it back with the position
+                // we got if it was a new thing, set the
+                // position to it default value
+                findNavController().navigate(DetailsFragmentDirections.actionDetailsDestinationToListingDestination()
+                    .setPosition(viewModel.position.value!!)
+                    .setShoe(Shoe(binding.editShoeName.text!!.toString(),
+                        binding.editShoeSize.text!!.toString().toDouble(),
+                        cieName,descript)))
 
                 // 2) Add it to the listing fragment
                 // Size x
