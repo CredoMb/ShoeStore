086e504128f4a27aa0cbc57632d8dd931c516820
Credo-Mbikayi <credo.mbikayi@umontreal.ca>
2021-05-31 18:06:02 -0400

    Remove the menu from the login fragment

--- app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
@@ -1,16 +1,10 @@
 package com.udacity.shoestore.screens.login
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
-import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.LoginFragmentBinding
@@ -22,47 +16,55 @@ class LoginFragment: Fragment() {
     private lateinit var viewModel: LoginViewModel
     private lateinit var viewModelFactory: LoginViewModelFactory
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true);
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
+
+
         val binding : LoginFragmentBinding =  DataBindingUtil.inflate(
-            inflater,
-            R.layout.login_fragment,
-            container,
-            false
+                inflater,
+                R.layout.login_fragment,
+                container,
+                false
         )
 
 
         binding.loginButton.setOnClickListener {
             goToWelcomeOrDisplayToast(!(binding.editPassword.text!!.isEmpty()) &&
-                                         !(binding.editUsername.text!!.isEmpty())
-                                     )
+                    !(binding.editUsername.text!!.isEmpty())
+            )
         }
 
         binding.signupButton.setOnClickListener {
             goToWelcomeOrDisplayToast(!(binding.editPassword.text!!.isEmpty()) &&
-                                         !(binding.editUsername.text!!.isEmpty())
-                                     )
+                    !(binding.editUsername.text!!.isEmpty())
+            )
         }
 
         return binding.root
     }
 
-    fun goToWelcomeOrDisplayToast(b:Boolean){
+    fun goToWelcomeOrDisplayToast(b: Boolean){
         if(b){
             findNavController().navigate(LoginFragmentDirections.actionLoginToWelcome())
         }
         else {
-            Toast.makeText(this.context,"Enter a username and password",Toast.LENGTH_LONG).show()
+            Toast.makeText(this.context, "Enter a username and password", Toast.LENGTH_LONG).show()
         }
     }
 
-    // You need some login informations to continue in this app
-
-
-    // define the behavior of the login button
-    // to first check if the "fields" are not empty
-    // then move the process forward
+    //Remove the menu from the
+    //fragment
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.clear()
+    }
 }
